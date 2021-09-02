ef0326adf5750f4fd86986e253540c602b95d7a8
Havyx <everluca@outlook.com>
2021-09-02 14:24:45 -0300

    :sparkles: logging flux test

--- src/test/java/dev/evertonsavio/app/webfluxtests/WebfluxTestsApplicationTests.java
+++ src/test/java/dev/evertonsavio/app/webfluxtests/WebfluxTestsApplicationTests.java
@@ -14,7 +14,7 @@ class WebfluxTestsApplicationTests {
 	@Test
 	void fluxTest(){
 		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
-				.concatWith(Flux.error(new RuntimeException("Exception Occurred")));
+				.concatWith(Flux.error(new RuntimeException("Exception Occurred"))).log();
 
 		stringFlux.subscribe(System.out::println, (e) -> System.err.println(e));
 	}
