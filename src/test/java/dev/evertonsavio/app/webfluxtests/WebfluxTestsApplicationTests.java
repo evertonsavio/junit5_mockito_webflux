c5ea4a19cabed75a66ecbc9db2fff591ed47675a
Havyx <everluca@outlook.com>
2021-09-02 14:23:34 -0300

    :sparkles: webflux first test

--- src/test/java/dev/evertonsavio/app/webfluxtests/WebfluxTestsApplicationTests.java
+++ src/test/java/dev/evertonsavio/app/webfluxtests/WebfluxTestsApplicationTests.java
@@ -2,12 +2,21 @@ package dev.evertonsavio.app.webfluxtests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+
 class WebfluxTestsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void fluxTest(){
+		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+				.concatWith(Flux.error(new RuntimeException("Exception Occurred")));
+
+		stringFlux.subscribe(System.out::println, (e) -> System.err.println(e));
 	}
 
 }
